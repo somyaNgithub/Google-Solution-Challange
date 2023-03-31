8838e33d824550b3d3ede5c0e94d5daf1948fe5a
unknown <somya.official19@gmail.com>
2023-03-31 23:35:26 +0530

    adding new files

---
+++ Hunger_Issue/README.md
@@ -0,0 +1,43 @@
+Hunger Free Web App
+Welcome to the Hunger Free web app, a platform for donating and delivering food to people in need. Our web app uses HTML and CSS in the front end, Django in the backend, and has integrated Metamask for ration tokens.
+
+Features
+Our web app offers the following features:
+
+Donating food: Users can donate food by filling out a form with the details of the food they want to donate.
+Requesting food: Users in need can request food by filling out a form with their details and the type of food they need.
+Delivery: Our platform connects donors with recipients and facilitates the delivery of donated food.
+Ration Tokens: Our web app integrates with Metamask to issue ration tokens to verified recipients. These tokens can be used to redeem food from our platform.
+
+Installation
+To run our web app, you will need to install the following:
+
+Python 3.7 or higher
+Django 3.1 or higher
+Metamask browser extension
+
+Once you have installed these dependencies, clone our repository and run the following command to start the development server:
+
+Copy code
+python manage.py runserver
+You should now be able to access the web app at http://localhost:8000/.
+
+Usage
+To use our web app, follow these steps:
+
+Connect your Metamask wallet to the web app.
+If you are a donor, fill out the food donation form with the details of the food you want to donate.
+If you are a recipient, fill out the food request form with your details and the type of food you need.
+Wait for our platform to connect you with a donor or recipient.
+If you are a recipient, you will receive ration tokens in your Metamask wallet. These tokens can be used to redeem food from our platform.
+
+Contribution
+If you would like to contribute to our web app, you can:
+
+Fork our repository
+Make changes and test them locally
+Submit a pull request
+We appreciate all contributions, big or small!
+
+License
+Our web app is licensed under the MIT License. See the LICENSE file for more information.
--- Hunger_Issue/website/views.py
+++ Hunger_Issue/website/views.py
@@ -1,5 +1,64 @@
 from django.shortcuts import render
+import pyrebase
 from .forms import MyModelForm
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+from django.contrib.auth.models import User
+
+
+config = {
+  "apiKey": "AIzaSyDJS6zb6mWXtZM__vtpncfRfjrmgtAzaqc",
+  "authDomain": "hackathonjaipur-c0ee0.firebaseapp.com",
+  "projectId": "hackathonjaipur-c0ee0",
+  "storageBucket": "hackathonjaipur-c0ee0.appspot.com",
+  "messagingSenderId": "154178091966",
+  "appId": "1:154178091966:web:a56dd0453f60b9d75cd7ef",
+  "measurementId": "G-S9116ES65H"
+}
+
+cred = credentials.Certificate('path/to/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            # User is authenticated
+        except auth.InvalidEmailError:
+            # Handle invalid email error
+        except auth.InvalidPasswordError:
+            # Handle invalid password error
+        except auth.UserNotFoundError:
+            # Handle user not found error
+
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            # User is authenticated
+            uid = user['localId']
+            # Check if the user exists in Django's database
+            django_user, created = User.objects.get_or_create(username=uid)
+            django_user.set_password(password)
+            django_user.save()
+            # Log in the user using Django's authentication system
+            auth_login(request, django_user)
+        except auth.InvalidEmailError:
+            # Handle invalid email error
+        except auth.InvalidPasswordError:
+            # Handle invalid password error
+        except auth.UserNotFoundError:
+            # Handle user not found error
+
+
 
 
 def index(request):
